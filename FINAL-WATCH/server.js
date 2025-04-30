1a7626ed711ad899669a97472d6b0cd908842695
aakash293 <90678921+aakash293@users.noreply.github.com>
2025-04-30 17:24:07 +0530

    Update server.js

--- FINAL-WATCH/server.js
+++ FINAL-WATCH/server.js
@@ -1,26 +1,47 @@
+// server.js
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const { Server } = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
+// Serve static files (index.html, room.html, etc.)
+app.use(express.static(path.join(__dirname)));
 
-io.on('connection', socket => {
-  socket.on('join', room => socket.join(room));
+// Root redirect to index.html (optional)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('join', (room) => {
+    socket.join(room);
+    console.log(User joined room: ${room});
+  });
 
   socket.on('chat', ({ room, msg }) => {
     io.to(room).emit('chat', msg);
   });
 
-  socket.on('video-control', data => {
+  socket.on('video-control', (data) => {
     socket.to(data.room).emit('video-control', data);
   });
 
-  socket.on('video-load', data => {
+  socket.on('video-load', (data) => {
     socket.to(data.room).emit('video-load', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
   });
 });
 
-server.listen(3000, () => console.log('Server running on http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(Server running on http://localhost:${PORT});
+});
