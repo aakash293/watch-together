4306f2f2b3fba3821afd97edd00c668fae6d666d
aakash293 <90678921+aakash293@users.noreply.github.com>
2025-05-01 12:24:58 +0530

    Update server.js

--- FINAL-WATCH/server.js
+++ FINAL-WATCH/server.js
@@ -1,4 +1,3 @@
-// server.js
 const express = require('express');
 const http = require('http');
 const path = require('path');
@@ -8,40 +7,53 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve static files (index.html, room.html, etc.)
 app.use(express.static(path.join(__dirname)));
 
-// Root redirect to index.html (optional)
+const roomUsers = {};
+
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('User connected');
 
-  socket.on('join', (room) => {
+  socket.on('join', ({ room, name }) => {
     socket.join(room);
-    console.log('User joined room: ${room}');
+    socket.room = room;
+    socket.name = name;
+
+    if (!roomUsers[room]) roomUsers[room] = {};
+    roomUsers[room][socket.id] = name;
+
+    io.to(room).emit('user-list', Object.values(roomUsers[room]));
   });
 
-  socket.on('chat', ({ room, msg }) => {
-    io.to(room).emit('chat', msg);
+  socket.on('ready', (room) => {
+    socket.to(room).emit('ready', socket.id);
   });
 
-  socket.on('video-control', (data) => {
-    socket.to(data.room).emit('video-control', data);
+  socket.on('offer', ({ target, offer }) => {
+    io.to(target).emit('offer', { from: socket.id, offer });
   });
 
-  socket.on('video-load', (data) => {
-    socket.to(data.room).emit('video-load', data);
+  socket.on('answer', ({ target, answer }) => {
+    io.to(target).emit('answer', { from: socket.id, answer });
+  });
+
+  socket.on('ice-candidate', ({ target, candidate }) => {
+    io.to(target).emit('ice-candidate', { from: socket.id, candidate });
   });
 
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
+    const room = socket.room;
+    if (room && roomUsers[room]) {
+      delete roomUsers[room][socket.id];
+      io.to(room).emit('user-list', Object.values(roomUsers[room]));
+    }
   });
 });
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log('Server running on http://localhost:${PORT}');
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
 });
