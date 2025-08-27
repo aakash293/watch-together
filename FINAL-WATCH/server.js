d6d77b336121b456bf337adb6478e497a351ac76
aakash293 <90678921+aakash293@users.noreply.github.com>
2025-08-27 13:29:42 +0530

    Update server.js

--- FINAL-WATCH/server.js
+++ FINAL-WATCH/server.js
@@ -218,7 +218,7 @@ io.on('connection', (socket) => {
 
 
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
